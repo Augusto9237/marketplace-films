717ed86863ff4e713ac4f3a56a4becd389337164
Augusto9237 <augusto.souza8330@gmail.com>
2024-01-03 02:03:27 -0300

    feat: add components Header, footer and navbar

--- angular-marketplace/src/app/app.component.ts
+++ angular-marketplace/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/template/header/header.component";
+import { FooterComponent } from "./components/template/footer/footer.component";
+import { NavComponent } from "./components/template/nav/nav.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, NavComponent]
 })
 export class AppComponent {
   title = 'angular-marketplace';
