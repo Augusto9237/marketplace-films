7cfba58d63c0099e8a3e177943bd0d2c7306b88c
Augusto9237 <augusto.souza8330@gmail.com>
2024-01-05 01:53:21 -0300

    feat: create List-films Component

--- angular-marketplace/src/app/views/list-films/list-films.component.ts
+++ angular-marketplace/src/app/views/list-films/list-films.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CardFilmComponent } from "../../components/template/card-film/card-film.component";
+import { MatCardModule } from '@angular/material/card';
+import { CheckoutService } from '../checkout/checkout.service';
+import { Film } from './film.model';
 
 @Component({
-    selector: 'app-list-films',
-    standalone: true,
-    templateUrl: './list-films.component.html',
-    styleUrl: './list-films.component.css',
-    imports: [CardFilmComponent]
+  selector: 'app-list-films',
+  standalone: true,
+  templateUrl: './list-films.component.html',
+  styleUrl: './list-films.component.css',
+  imports: [CardFilmComponent, MatCardModule]
 })
-export class ListFilmsComponent {
+export class ListFilmsComponent implements OnInit {
+  listFilms: Film[] = []
 
+  constructor(private checkoutService: CheckoutService) {}
+
+  ngOnInit(): void {
+    this.checkoutService.getListFilms().subscribe((film) => {
+      this.listFilms = film;
+    })
+  }
 }
